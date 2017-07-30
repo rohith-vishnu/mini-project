d9126a38e0c79f4b018416980630cc7d0aa97938
rohith1 <karanamrohith@gmail.com>
2017-07-30 13:48:18 +0530

    store urls in txt file

--- browser history.py
+++ browser history.py
@@ -9,11 +9,16 @@ history_db = os.path.join(data_path,'history')
 
 c= sqlite3.connect(history_db)
 cursor=c.cursor()
-select_statement= "SELECT urls.url,urls.Visit_count FROM urls,Visits WHERE urls.id=visits.url;"
+select_statement= "SELECT urls.url FROM urls,Visits WHERE urls.id=visits.url;"
 
 cursor.execute(select_statement)
 
 result=cursor.fetchall()
+result=list(set(result))
+file=open("URLS.txt","w")
 
-print(result)
+file.writelines(["%s\n" % item  for item in result])
+
+
+
 
